f6c157c07ec7fc92ac0292d56eb6838db78fea2b
vbekris <vasbekris@gmail.com>
2025-11-05 19:35:53 +0200

    searchAgents

--- searchAgents.py
+++ searchAgents.py
@@ -296,14 +296,20 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        visited = []
+        for corner in self.corners:
+                if(self.startingPosition == corner):
+                    visited.append(True)
+                else:
+                    visited.append(False)
+        return (self.startingPosition, tuple(visited))
 
     def isGoalState(self, state: Any):
-        """
+        """ 
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        pos , expl = state
+        return all(expl)
 
     def getSuccessors(self, state: Any):
         """
@@ -320,10 +326,11 @@ class CornersProblem(search.SearchProblem):
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             # Add a successor state to the successor list if the action is legal
             # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
+            currentPosition, explored = state
+            x,y = currentPosition
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
 
